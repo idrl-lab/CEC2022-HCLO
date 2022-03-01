9b599ce5971818840eb075b404595ed0507215a0
loriame <1007574921@qq.com>
2022-03-01 11:25:04 +0800

    修改内容

--- js/statistics.js
+++ js/statistics.js
@@ -48,6 +48,13 @@ window.downloadSubmit = function () {
     return;
   }
 
+  const email_re =
+    /^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@([A-Za-z\d]+[-.])+[A-Za-z\d]{2,4}$/;
+  if (!email_re.test(email)) {
+    alert("The mailbox format is incorrect");
+    return;
+  }
+
   const formData = new FormData();
   formData.append("name", name);
   formData.append("institution", institution);
